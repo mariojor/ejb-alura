763eee46f309e910e938855042a8d0b968bab1d0
Mario Jorge <mariojorge.sdei@gmail.com>
2017-02-13 22:20:53 -0300

    EJB - alura

---
+++ livraria/src/br/com/caelum/livraria/dao/AutorDao.java
@@ -0,0 +1,33 @@
+package br.com.caelum.livraria.dao;
+
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.interceptor.Interceptors;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import br.com.caelum.livraria.interceptador.LogInterceptador;
+import br.com.caelum.livraria.modelo.Autor;
+
+@Stateless
+//@Interceptors({LogInterceptador.class})
+public class AutorDao {
+
+	@PersistenceContext
+	private EntityManager manager;
+
+	public void salva(Autor autor) {
+		manager.persist(autor);
+	}
+	
+	public List<Autor> todosAutores() {
+		return manager.createQuery("select a from Autor a", Autor.class).getResultList();
+	}
+
+	public Autor buscaPelaId(Integer autorId) {
+		Autor autor = this.manager.find(Autor.class, autorId);
+		return autor;
+	}
+	
+}
